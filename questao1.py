1ea7427740d320446aea218e42dfd7302ef89521
Leonardo Avellar <leonardoggavellar@gmail.com>
2017-09-08 18:23:44 -0300

    Reorganização da questão 1

--- questao1.py
+++ questao1.py
@@ -1,17 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*-
-"""
-Questão 1: Descreva um algoritmo (o mais eficiente possível) que, dado um grafo por sua matriz de adjacências, determina sua lista de adjacências. Qual é a complexidade desse algoritmo? Ele funciona para digrafos?
-    
-Ideia do algoritmo:
-listaAdj = lista adjacências
-para cada linha I na matriz:
-	para cada coluna J na linha i:
-		se matriz[linhaI][colunaJ] != 0:
-                        adjList[linhaI].append(colunaJ)
-
-Exemplo abaixo. Ele não está o mais otimizado para facilitar a visualização. Sua complexidade é linear; O(n). Sim, funciona para grafos direcionados e para grafos não direcionados.
-"""
 def matrizAdj2listaAdj(matriz):
     listaAdj = []
     posLinha = posColuna = 0
